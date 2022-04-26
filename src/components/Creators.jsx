718e9c939eb365ae616f74e9d564dc3210cc7966
John Murphy <jmurphy5613@gmail.com>
2022-04-26 16:41:24 -0400

    here we go again

---
+++ src/components/Creators.jsx
@@ -0,0 +1,20 @@
+
+
+
+const Creators = () => {
+    return (
+        <div style={{
+            position: 'absolute',
+            top: '0',
+            right: '1rem',
+        }}>
+            <h3 style={{
+                fontFamily: 'Inter, sans-serif',
+            }}>Created by the <a style={{
+                color: '#900000'
+            }}>Fordian</a> Staff</h3>
+        </div>
+    )
+}
+
+export default Creators;
