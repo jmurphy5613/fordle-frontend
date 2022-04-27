ca36a91ef4eb76d891b6631b1509dd5552c11759
John Murphy <jmurphy5613@gmail.com>
2022-04-26 21:00:46 -0400

    finished proof of concept

--- src/Words.js
+++ src/Words.js
@@ -1,4 +1,5 @@
 import wordBank from "./wordle-bank.txt";
+import axios from "axios";
 
 export const boardDefault = [
   ["", "", "", "", ""],
@@ -12,12 +13,19 @@ export const boardDefault = [
 export const generateWordSet = async () => {
   let wordSet;
   let todaysWord;
+
+  await axios.get('http://localhost:3001/words/currentWord').then(res => {
+    todaysWord = res.data.word;
+  });
+
+
   await fetch(wordBank)
     .then((response) => response.text())
     .then((result) => {
       const wordArr = result.split("\r\n")
-      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
       wordSet = new Set(wordArr);
     });
+  
+    console.log(todaysWord);
   return { wordSet, todaysWord };
 };
